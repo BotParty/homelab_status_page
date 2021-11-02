932ddb2d1ecb4af1a6779808efeb03007d69f413
eggnog <eggnog@nyc-map.com>
2021-11-02 18:18:23 -0400

    test

--- main.js
+++ main.js
@@ -1,30 +1,39 @@
 (function main() {
-  let gl = {};
-
-  let canvas = document.createElement("canvas");
-
-  let DOM = {
-    canvas: canvas,
+  const recordRenderPass = async function (stuff) {
+    let {
+      gpuDevice,
+      context,
+      renderPassDescriptor,
+      pipeline,
+      uniformsBuffer,
+      attribsBuffer,
+    } = stuff;
+    const commandEncoder = gpuDevice.createCommandEncoder();
+    const textureView = context.getCurrentTexture().createView();
+    renderPassDescriptor.colorAttachments[0].view = textureView;
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.setPipeline(pipeline);
+    const bindGroup = gpuDevice.createBindGroup({
+      layout: pipeline.getBindGroupLayout(0),
+      entries: [
+        {
+          binding: 0,
+          resource: {
+            buffer: uniformsBuffer,
+          },
+        },
+      ],
+    });
+    //let videoBindGroup = missingStuff(stuff)
+    passEncoder.setBindGroup(0, bindGroup);
+    //passEncoder.setBindGroup(1, videoBindGroup);
+    passEncoder.setVertexBuffer(0, attribsBuffer);
+    passEncoder.draw(3 * 2, 1, 0, 0);
+    passEncoder.endPass();
+    gpuDevice.queue.submit([commandEncoder.finish()]); //async
   };
-
-  const width = innerWidth,
-    height = innerHeight;
-
-  const data = {
-    mouseX: 0,
-    mouseY: 0,
-    angle: 0,
-  };
-  //
-  setInterval(function () {
-    data.angle = (data.angle + 5) % 360;
-  }, 16);
-  //uniformsArray
-  //(dimensions.xyz), (time, angle), (mouseX, mouseY)
-  //(0,       1,  2)  (2,   3),      (5,          6)
   function updateUniforms(stuff) {
     let {
-      uniformsArray,
       data,
       gpuDevice,
       uniformsBuffer,
@@ -33,148 +42,17 @@
       pipeline,
       attribsBuffer,
     } = stuff;
-
-    uniformsArray.set(
-      [performance.now() / 1000, data.angle, data.mouseX, data.mouseY],
-      3,
-      6
-    );
-    uniformsBuffer = createBuffer(
+    let values = Object.values(data);
+    let uniformsArray = new Float32Array(values.length);
+    uniformsArray.set(values, 0, values.length);
+    return createBuffer(
       gpuDevice,
       uniformsArray,
       GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
     );
-    return uniformsBuffer;
   }
-
-  const createBuffer = (gpuDevice, arr, usage) => {
-    let desc = {
-      size: (arr.byteLength + 3) & ~3,
-      usage,
-      mappedAtCreation: true,
-    };
-    let buffer = gpuDevice.createBuffer(desc);
-    const writeArray =
-      arr instanceof Uint16Array
-        ? new Uint16Array(buffer.getMappedRange())
-        : new Float32Array(buffer.getMappedRange());
-    writeArray.set(arr);
-    buffer.unmap();
-    return buffer;
-  };
-
-  canvas.addEventListener("mousemove", function (e) {
-    data.mouseX = e.clientX / innerWidth;
-    data.mouseY = e.clientY / innerHeight;
-  });
-
-  function makeShaderModule(gpuDevice, uniforms, name, sources, inputs) {
-    let source = `
-  let size = 3.0;
-
-  // fn mandelbrot(p: vec2<f32>) -> vec2<f32> {
-  //   let a: i32 = 2;
-  //   let b: f32 = 2.0;
-  // 	var z = vec4<f32>(0.0, 0.0);
-  // 	var c = vec4<f32>(p.xy * 2.0);
-  //   let MAX_ITERATIONS = 100;
-  //   let epsilon = 10.0;
-  // 	for (int i = 0; i < MAX_ITERATIONS; i++) {
-  // 		if (length(z) > epsilon) {
-  // 			return (float(i) / 50.0) + vec2<f32>(0.25);
-  // 		}
-  // 		z = vec2<f32>(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + c;
-  // 	}
-  // 	return vec3<f32>(0.0, 0.0, 0.0);
-  // }
-
-  fn main(uv: vec2<f32>) -> vec4<f32> {
-    var base = vec4<f32>(.5, .5, .9, 1.);
-    let dist = distance( uv, vec2<f32>(u.mouseX, u.mouseY));
-    if (uv.x < .1 ) { base.y = .1; }
-    if (uv.x > .2 ) { base.y = .5; }
-
-    if (uv.x > .3 ) { base.x = .5; }
-
-    if (uv.x > .4 ) { base.z = .7; }
-
-    if (uv.x > .5 ) { base.z = .9; }
-
-    if (dist < .1) { base.x = .1; }
-    return base;
-  }
-
-  [[stage(fragment)]]
-  fn main_fragment(in: VertexOutput) -> [[location(0)]] vec4<f32> {
-    let x = u.resolution;
-    return main(in.uv);
-  }
-  `;
-    const userland_Uniforms = Object.keys(uniforms)
-      .map((name) => `${name}: f32;`)
-      .join("\n");
-
-    const input = Object.keys(inputs)
-      .map((name) => `${name}: f32;`)
-      .join("\n");
-
-    const shader = gpuDevice.createShaderModule({
-      code: `
-      [[block]] struct Uniforms {
-        resolution: vec3<f32>;
-        time: f32;
-        ${userland_Uniforms}
-        ${input}
-      };
-
-  [[group(0), binding(0)]] var<uniform> u: Uniforms;
-
-  struct VertexInput {
-  [[location(0)]] pos: vec2<f32>;
-  };
-  //uv coordinates seem to be off...
-  struct VertexOutput {
-  [[builtin(position)]] pos: vec4<f32>;
-  [[location(0)]] uv: vec2<f32>;
-  };
-
-  [[stage(vertex)]]
-  fn main_vertex(input: VertexInput) -> VertexOutput {
-    var output: VertexOutput;
-    var pos: vec2<f32> = input.pos * 3.0 - 1.0;
-    output.pos = vec4<f32>(pos, 0.0, 1.0);
-    output.uv = input.pos;
-    return output;
-  }
-  ${[...sources].join("\n")}
-  ${source}
-  `,
-    });
-    return shader;
-  }
-  async function init(stuff) {
-    //top level
-    let { uniforms = {}, inputs = {}, sources = [] } = stuff;
-    const context = canvas.value || canvas.getContext("webgpu");
-
-    const adapter = await navigator.gpu.requestAdapter();
-
-    const gpuDevice = await adapter.requestDevice();
-    const presentationFormat = context.getPreferredFormat(adapter);
-
-    const presentationSize = [
-      canvas.clientWidth * devicePixelRatio,
-      canvas.clientHeight * devicePixelRatio,
-    ];
-    context.configure({
-      device: gpuDevice,
-      format: presentationFormat,
-      size: presentationSize,
-    });
-
-    let shader = makeShaderModule(gpuDevice, uniforms, name, sources, inputs);
-
-    const pipeline = gpuDevice.createRenderPipeline({
+  function makePipeline(shader, gpuDevice) {
+    let pipelineDesc = {
       vertex: {
         module: shader,
         entryPoint: "main_vertex",
@@ -199,7 +77,117 @@
       primitives: {
         topology: "triangle-list",
       },
+    };
+    return gpuDevice.createRenderPipeline(pipelineDesc);
+  }
+
+  const createBuffer = (gpuDevice, arr, usage) => {
+    let desc = {
+      size: (arr.byteLength + 3) & ~3,
+      usage,
+      mappedAtCreation: true,
+    };
+    let buffer = gpuDevice.createBuffer(desc);
+    const writeArray =
+      arr instanceof Uint16Array
+        ? new Uint16Array(buffer.getMappedRange())
+        : new Float32Array(buffer.getMappedRange());
+    writeArray.set(arr);
+    buffer.unmap();
+    return buffer;
+  };
+
+  function makeShaderModule(gpuDevice, data, name, sources) {
+    let source = `
+    let size = 3.0;
+  fn main(uv: vec2<f32>) -> vec4<f32> {
+    var base = vec4<f32>(.5, .5, .9, 1.);
+    let dist = distance( uv, vec2<f32>(u.mouseX, u.mouseY));
+    if (uv.x < .1 ) { base.y = .1; }
+    if (uv.x > .2 ) { base.y = .5; }
+    if (uv.x > .3 ) { base.x = .5; }
+    if (uv.x > .4 ) { base.z = .7; }
+    if (uv.x > .5 ) { base.z = .9; }
+    if (dist < .1) { base.x = .1; }
+    return base;
+  }
+
+  [[stage(fragment)]]
+  fn main_fragment(in: VertexOutput) -> [[location(0)]] vec4<f32> {
+    return main(in.uv);
+  }
+  `;
+    const userland_Uniforms = Object.keys(data)
+      .map((name) => `${name}: f32;`)
+      .join("\n");
+
+    const shader = gpuDevice.createShaderModule({
+      code: `
+      [[block]] struct Uniforms {
+        ${userland_Uniforms}
+      };
+
+  [[group(0), binding(0)]] var<uniform> u: Uniforms;
+  struct VertexInput {
+  [[location(0)]] pos: vec2<f32>;
+  };
+  struct VertexOutput {
+  [[builtin(position)]] pos: vec4<f32>;
+  [[location(0)]] uv: vec2<f32>;
+  };
+
+  [[stage(vertex)]]
+  fn main_vertex(input: VertexInput) -> VertexOutput {
+    var output: VertexOutput;
+    var pos: vec2<f32> = input.pos * 3.0 - 1.0;
+    output.pos = vec4<f32>(pos, 0.0, 1.0);
+    output.uv = input.pos;
+    return output;
+  }
+  ${source}`,
     });
+    return shader;
+  }
+  //generic functions above
+  //userland code
+  let canvas = document.createElement("canvas");
+  let DOM = { canvas: canvas };
+  const width = innerWidth,
+    height = innerHeight;
+  let data = {
+    width: 900,
+    height: 500,
+    pixelRatio: 2,
+    time: 0,
+    mouseX: 0,
+    mouseY: 0,
+    angle: 0,
+  };
+  canvas.addEventListener("mousemove", function (e) {
+    data.mouseX = e.clientX / innerWidth;
+    data.mouseY = e.clientY / innerHeight;
+  });
+  //end userland
+
+  async function init(stuff) {
+    //bplate
+    const context = canvas.value || canvas.getContext("webgpu");
+    const adapter = await navigator.gpu.requestAdapter();
+    const gpuDevice = await adapter.requestDevice();
+    const presentationFormat = context.getPreferredFormat(adapter);
+    const presentationSize = [
+      canvas.clientWidth * devicePixelRatio,
+      canvas.clientHeight * devicePixelRatio,
+    ];
+    context.configure({
+      device: gpuDevice,
+      format: presentationFormat,
+      size: presentationSize,
+    });
+
+    let shader = makeShaderModule(gpuDevice, data, name);
+
+    const pipeline = makePipeline(shader, gpuDevice);
     const textureView = context.getCurrentTexture().createView();
     const renderPassDescriptor = {
       colorAttachments: [
@@ -210,59 +198,18 @@
         },
       ],
     };
+    //
     const attribs = new Float32Array([0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1]);
     const attribsBuffer = createBuffer(
       gpuDevice,
       attribs,
       GPUBufferUsage.VERTEX
     );
-
-    let uniformsArray = new Float32Array([
-      width, // res.x
-      height, // res.y
-      window.devicePixelRatio, // res.z
-      0, // time
-      ...Object.values(uniforms),
-      ...Array.from(Object.values(inputs), (input) => input.value),
-    ]);
-
-    async function recordRenderPass(stuff) {
-      let {
-        gpuDevice,
-        context,
-        renderPassDescriptor,
-        pipeline,
-        uniformsBuffer,
-        attribsBuffer,
-      } = stuff;
-      const commandEncoder = gpuDevice.createCommandEncoder();
-      const textureView = context.getCurrentTexture().createView();
-      renderPassDescriptor.colorAttachments[0].view = textureView;
-      const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-      passEncoder.setPipeline(pipeline);
-      const bindGroup = gpuDevice.createBindGroup({
-        layout: pipeline.getBindGroupLayout(0),
-        entries: [
-          {
-            binding: 0,
-            resource: {
-              buffer: uniformsBuffer,
-            },
-          },
-        ],
-      });
-
-      passEncoder.setBindGroup(0, bindGroup);
-      passEncoder.setVertexBuffer(0, attribsBuffer);
-      passEncoder.draw(3 * 2, 1, 0, 0);
-      passEncoder.endPass();
-      gpuDevice.queue.submit([commandEncoder.finish()]); //async
-    }
+    //wrap make attributes
 
     function draw(ctx) {
       let uniformsBuffer = updateUniforms({
-        uniformsArray,
-        data,
+        data: ctx.data,
         gpuDevice,
         context,
         renderPassDescriptor,
@@ -282,17 +229,14 @@
     return draw;
   }
   async function run() {
-    let options = {
-      uniforms: data,
-    };
+    let copiedData = Object.assign({}, data);
+    copiedData.time = performance.now();
+    let options = { data: copiedData };
     let draw = await init(options);
-    setInterval(draw, 100);
+    draw(options);
+    //setInterval(draw, 100);
   }
-
-  //if observable  return canvas;
-  //if vite
   let isWithinWorkerScope = true;
-  console.log(window.location.host);
   let host = window.location.host;
   if (host !== "localhost:3000") {
     run();
@@ -301,7 +245,7 @@
     requestAnimationFrame(async function () {
       let div = document.body;
       div.insertBefore(canvas, document.querySelector("img"));
-      await run();
+      run();
     });
   }
 }.call(this));
