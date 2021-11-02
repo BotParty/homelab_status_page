f9153b8d9e58cc16cf2ab51e55ad0ccfa27f5735
eggnog <eggnog@nyc-map.com>
2021-11-01 20:37:43 -0400

    save state

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 import "./style.css";
-//make it so simple a 7 year old can understand it.
-const width = 640,
-  height = 480;
+//want fragCoord to do stuff with mouse
+//otherwise fragment has no idea what it's distance to the cursor is at all.
+//fragCoord is a vertex attribute not uniform
+//could just add an array from 0-size of list
+//and the vertex shader will naturally interpolate the data
+
+//with 1 more thing, anyone can add anything in shadertoy
+//mousePosition, fragPosition and
+
+//(insert whatever uniforms desired here) (numbers only for now )
+//texture data next (still numbers but more flexible than vertices because compute shaders can do stuff )
+const width = innerWidth,
+  height = innerHeight;
 
 const data = {
   mouseX: 0,
   mouseY: 0,
-  angle: 50
+  angle: 0
 };
+//
+setInterval(function() {
+  data.angle = (data.angle + 5) % 360;
+}, 16);
 //uniformsArray
 //(dimensions.xyz), (time, angle), (mouseX, mouseY)
-//(0, 1, 2)         (2 , 3),  4 , (5, 6)
+//(0,       1,  2)  (2,   3),      (5,          6)
 function updateUniforms(stuff) {
   let {
     uniformsArray,
@@ -24,12 +38,7 @@ function updateUniforms(stuff) {
   } = stuff;
 
   uniformsArray.set(
-    [
-      performance.now() / 1000,
-      data.angle + Math.random(),
-      data.mouseX,
-      data.mouseY
-    ],
+    [performance.now() / 1000, data.angle, data.mouseX, data.mouseY],
     3,
     6
   );
@@ -61,46 +70,49 @@ const canvas = document.querySelector("canvas");
 canvas.addEventListener("mousemove", function(e) {
   data.mouseX = e.clientX / innerWidth;
   data.mouseY = e.clientY / innerHeight;
-  //console.log("hi", data.mouseX, data.mouseY);
 });
 canvas.style = `max-width: 100%; width: ${width}px; height: auto;`;
 
 function makeShaderModule(device, uniforms, name, sources, inputs) {
   let source = `
-fn rotate2d(a: f32) -> mat2x2<f32> {
-let c = cos(a);
-let s = sin(a);
-return mat2x2<f32>(
-    vec2<f32>(c, -s),
-    vec2<f32>(s, c)
-);
-}
-
-let size = 15.0;
+let size = 3.0;
 
 fn main(uv: vec2<f32>) -> vec4<f32> {
-  var p = (uv - 0.5) * (u.resolution.xy) * rotate2d(u.angle);
-  if (p.x < 0.0) {p.x = p.x - size;}
-  if (p.y < 0.0) {p.y = p.y - size;}
-  p = abs(p);
-  let q = p.x % (size * 2.0) < size == p.y % (size * 2.0) < size;
-  let o = f32(q);
-  return vec4<f32>(o, o, o, 1.0);
+  var base = vec4<f32>(.5, .5, .9, 1.);
+  let dist = distance(vec2<f32>(u.mouseX, u.mouseY), uv);
+  // var p = (uv - 0.3) * (u.resolution.xy) * rotate2d(u.angle);
+  // if (p.x < 10.0) {p.x = p.x - size;}
+  // if (p.y < 5.0) {p.y = p.y - size;}
+  // let q = p.x % (size * 2.0) < size == p.y % (size * 2.0) < size;
+  // var o = f32(q);
+  if (dist < .5) { return vec4<f32>(.0, .4, .9, 1.); }
+  //
+
+  return base;
+}
+
+[[stage(fragment)]]
+fn main_fragment(in: VertexOutput) -> [[location(0)]] vec4<f32> {
+  let x = u.resolution;
+  return main(in.uv);
 }
 `;
+  const userland_Uniforms = Object.keys(uniforms)
+    .map(name => `${name}: f32;`)
+    .join("\n");
+
+  const input = Object.keys(inputs)
+    .map(name => `${name}: f32;`)
+    .join("\n");
 
   const shader = device.createShaderModule({
     code: `
     [[block]] struct Uniforms {
-    resolution: vec3<f32>;
-     time: f32;
-${Object.keys(uniforms)
-      .map(name => `${name}: f32;`)
-      .join("\n")}
-${Object.keys(inputs)
-      .map(name => `${name}: f32;`)
-      .join("\n")}
-};
+      resolution: vec3<f32>;
+      time: f32;
+      ${userland_Uniforms}
+      ${input}
+    };
 
 [[group(0), binding(0)]] var<uniform> u: Uniforms;
 
@@ -115,20 +127,15 @@ struct VertexOutput {
 
 [[stage(vertex)]]
 fn main_vertex(input: VertexInput) -> VertexOutput {
-var output: VertexOutput;
-var pos: vec2<f32> = input.pos * 2.0 - 1.0;
-output.pos = vec4<f32>(pos, 0.0, 1.0);
-output.uv = input.pos;
-return output;
+  var output: VertexOutput;
+  var pos: vec2<f32> = input.pos * 3.0 - 1.0;
+  output.pos = vec4<f32>(pos, 0.0, 1.0);
+  output.uv = input.pos;
+  return output;
 }
-
 ${[...sources].join("\n")}
 ${source}
-[[stage(fragment)]]
-fn main_fragment(in: VertexOutput) -> [[location(0)]] vec4<f32> {
-let x = u.resolution; // need to use all inputs
-return main(in.uv) + vec4<f32>(0., u.mouseX, u.mouseY, 0.);
-}`
+`
   });
   return shader;
 }
@@ -228,7 +235,6 @@ async function init(stuff) {
 
   function draw(ctx) {
     let uniformsBuffer = updateUniforms({
-      //needs an upload spot
       uniformsArray,
       data,
       device,
@@ -256,7 +262,7 @@ async function dot() {
     uniforms: data
   };
   let draw = await init(options);
-  setInterval(draw, 150);
+  setInterval(draw, 19);
 }
 
 dot();
