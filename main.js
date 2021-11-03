777167ca48604ce968396356e16ed42d8fbdb2d0
eggnog <eggnog@nyc-map.com>
2021-11-02 21:36:25 -0400

    save state

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
 (function main() {
+  function missingStuff(stuff) {
+    let { gpuDevice, pipeline, video } = stuff;
+    console.log(stuff);
+    const sampler = gpuDevice.createSampler({
+      magFilter: "linear",
+      minFilter: "linear",
+    });
+    console.log(video);
+    const uniformBindGroup = gpuDevice.createBindGroup({
+      layout: pipeline.getBindGroupLayout(1),
+      entries: [
+        {
+          binding: 0,
+          resource: sampler,
+        },
+        {
+          binding: 1,
+          resource: gpuDevice.importExternalTexture({
+            source: video,
+          }),
+        },
+      ],
+    });
+    return uniformBindGroup;
+  }
+
+  const webGPUTextureFromImageUrl = async function (gpuDevice, url) {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const imgBitmap = await createImageBitmap(blob);
+
+    return webGPUTextureFromImageBitmapOrCanvas(gpuDevice, imgBitmap);
+  };
   const recordRenderPass = async function (stuff) {
     let {
       gpuDevice,
@@ -13,6 +46,7 @@
     renderPassDescriptor.colorAttachments[0].view = textureView;
     const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
     passEncoder.setPipeline(pipeline);
+    gpuDevice.createB;
     const bindGroup = gpuDevice.createBindGroup({
       layout: pipeline.getBindGroupLayout(0),
       entries: [
@@ -24,9 +58,10 @@
         },
       ],
     });
-    //let videoBindGroup = missingStuff(stuff)
+
+    let videoBindGroup = missingStuff(stuff);
     passEncoder.setBindGroup(0, bindGroup);
-    //passEncoder.setBindGroup(1, videoBindGroup);
+    passEncoder.setBindGroup(1, videoBindGroup);
     passEncoder.setVertexBuffer(0, attribsBuffer);
     passEncoder.draw(3 * 2, 1, 0, 0);
     passEncoder.endPass();
@@ -101,14 +136,14 @@
     let source = `
     let size = 3.0;
   fn main(uv: vec2<f32>) -> vec4<f32> {
-    var base = vec4<f32>(.5, .5, .9, 1.);
+    var base = vec4<f32>(cos(u.time), .5, sin(u.time), 1.);
     let dist = distance( uv, vec2<f32>(u.mouseX, u.mouseY));
     if (uv.x < .1 ) { base.y = .1; }
     if (uv.x > .2 ) { base.y = .5; }
     if (uv.x > .3 ) { base.x = .5; }
-    if (uv.x > .4 ) { base.z = .7; }
-    if (uv.x > .5 ) { base.z = .9; }
-    if (dist < .1) { base.x = .1; }
+    //if (uv.x > .4 ) { base.z = .7; }
+    //if (uv.x > .5 ) { base.z = .9; }
+    //if (dist < .1) { base.x = .1; }
     return base;
   }
 
@@ -123,17 +158,18 @@
 
     const shader = gpuDevice.createShaderModule({
       code: `
-      [[block]] struct Uniforms {
-        ${userland_Uniforms}
-      };
-
+  [[block]] struct Uniforms {
+    ${userland_Uniforms}
+  };
   [[group(0), binding(0)]] var<uniform> u: Uniforms;
+  [[group(1), binding(0)]] var mySampler: sampler;
+  [[group(1), binding(1)]] var myTexture: texture_external;
   struct VertexInput {
-  [[location(0)]] pos: vec2<f32>;
+    [[location(0)]] pos: vec2<f32>;
   };
   struct VertexOutput {
-  [[builtin(position)]] pos: vec4<f32>;
-  [[location(0)]] uv: vec2<f32>;
+    [[builtin(position)]] pos: vec4<f32>;
+    [[location(0)]] uv: vec2<f32>;
   };
 
   [[stage(vertex)]]
@@ -149,8 +185,6 @@
     return shader;
   }
   //generic functions above
-  //userland code.. move
-  //end userland
   async function init(stuff) {
     //bplate
     const context = stuff.canvas.value || stuff.canvas.getContext("webgpu");
@@ -166,9 +200,7 @@
       format: presentationFormat,
       size: presentationSize,
     });
-
     let shader = makeShaderModule(gpuDevice, data, name);
-
     const pipeline = makePipeline(shader, gpuDevice);
     const textureView = context.getCurrentTexture().createView();
     const renderPassDescriptor = {
@@ -180,7 +212,6 @@
         },
       ],
     };
-    //
     const attribs = new Float32Array([0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1]);
     const attribsBuffer = createBuffer(
       gpuDevice,
@@ -188,10 +219,11 @@
       GPUBufferUsage.VERTEX
     );
     //wrap make attributes
+    function draw(stuff) {
+      console.log(stuff);
 
-    function draw(ctx) {
       let uniformsBuffer = updateUniforms({
-        data: ctx.data,
+        data: stuff.data,
         gpuDevice,
         context,
         renderPassDescriptor,
@@ -206,48 +238,88 @@
         pipeline,
         uniformsBuffer,
         attribsBuffer,
+        video: stuff.video,
       }).finally(() => {});
+
+      return stuff;
     }
     return draw;
   }
-
   //userland
   let data = {
-    width: 900,
-    height: 500,
-    pixelRatio: 2,
-    time: 0,
+    width: 900, //based on canvas
+    height: 500, //based on canvas
+    pixelRatio: 2, //based on canvas
+    time: 0, //...time travel... open question = how clock should interact with gpu.
     mouseX: 0,
     mouseY: 0,
     angle: 0,
-    //texture: video
+    //texture: (video)
   };
-  function run() {
+
+  //run takes in a stuff object
+  //which has data and a canvas
+  function* hasReturnValue() {
+    yield "a";
+    yield "b";
+    return "The result";
+  }
+
+  const width = 960,
+    height = 500;
+  //above = regl
+  async function start_loop() {
     const canvas = document.createElement("canvas");
-    console.log(canvas);
-    const width = 960,
-      height = 500;
     canvas.addEventListener("mousemove", function (e) {
       data.mouseX = e.clientX / width;
       data.mouseY = e.clientY / height;
     });
-    let copiedData = Object.assign({}, data);
-    copiedData.time = performance.now();
+    let copiedData = Object.assign({}, data); //should come from args
+    copiedData.time = Date.now() % 1000; //le clock
     let stuff = { data: copiedData, canvas: canvas, width, height };
-    init(stuff).then((draw) => draw(stuff));
-    //inbetween these two placxrqes ... do things that mutate the stuff
-    //setInterval(draw, 100);
-    return canvas;
+    stuff.video = createVideo();
+    let video = stuff.video;
+    await video.play();
+
+    let draw = await init(stuff);
+    let transformedStuff = draw(stuff);
+    return transformedStuff;
   }
 
-  let isWithinWorkerScope = true;
   let host = window.location.host;
   if (host !== "localhost:3000") {
-    return run();
+    console.log("obs-land");
+    start_loop().then((stuff) => {
+      console.log(stuff);
+    });
   } else {
+    console.log("atom-land");
     requestAnimationFrame(async function () {
-      let canvas = run();
-      document.body.insertBefore(canvas, document.querySelector("img"));
+      let canvas = start_loop().then((stuff) => {
+        document.body.append(stuff.canvas);
+      });
     });
   }
 }.call(this));
+
+import video_src from "./data/big-buck-bunny_trailer.webm";
+
+function createVideo() {
+  console.log("123");
+  const video = document.createElement("video");
+  video.loop = true;
+  video.autoplay = true;
+  video.muted = true;
+  video.width = "480";
+  video.height = "270";
+  video.currentTime = 15;
+  video.loop = true;
+  video.crossorigin = "anonymous";
+  video.controls = "true";
+  video.src = video_src;
+  console.log(video_src);
+  //http://jplayer.org/video/webm/Big_Buck_Bunny_Trailer.webm
+  //await video.play();
+  document.body.appendChild(video);
+  return video;
+}
