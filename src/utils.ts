c7ca8289acbbca4196823e60bdd86cfb01b4aa2e
eggnog <eggnog@nyc-map.com>
2021-11-22 13:47:50 -0800

    remove files

--- src/utils.ts
+++ src/utils.ts
@@ -1,38 +1,5 @@
 const attribs = new Float32Array([0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1]);
 
-let default_source = `
-    let size = 3.0;
-    let b = 0.003;		//size of the smoothed border
-    fn mainImage(fragCoord: vec2<f32>, iResolution: vec2<f32>) -> vec4<f32> {
-      let aspect = iResolution.x/iResolution.y;
-      let position = (fragCoord.xy/iResolution.xy) * aspect;
-      let dist = distance(position, vec2<f32>(aspect*0.5, 0.5));
-      let offset=u.time;
-      let conv=4.;
-      let v=dist*4.-offset;
-      let ringr=floor(v);
-      //let color=smoothstep(-b, b, abs(dist- (ringr+float(fract(v)>0.5)+offset)/conv));
-      //let color=smoothstep(-b, b, abs(dist- (ringr+((v)>0.5)+offset)/conv));
-      var color = b;
-      if (ringr % 2. ==1.) {
-       color=2.-color;
-      }
-    return vec4<f32>(.5, color, color, 1.);
-  };
-
-
-  fn main(uv: vec2<f32>) -> vec4<f32> {
-    let fragCoord = vec2<f32>(uv.x, uv.y);
-    var base = vec4<f32>(cos(u.time), .5, sin(u.time), 1.);
-    let dist = distance( fragCoord, vec2<f32>(u.mouseX,  u.mouseY));
-    return vec4<f32>(.3, .3, sin(u.time), 1.) + mainImage(fragCoord, vec2<f32>(u.width, u.height));
-  }
-
-  [[stage(fragment)]]
-  fn main_fragment(in: VertexOutput) -> [[location(0)]] vec4<f32> {
-    return main(in.uv);
-  }
-  `;
 let data = {
   width: innerWidth, //based on canvas
   height: innerHeight, //based on canvas
@@ -92,12 +59,10 @@ const recordRenderPass = async function (stuff) {
 
   let passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
 
-
   if (! stuff.renderBundle)
     passEncoder = stuff.gpuDevice.createRenderBundleEncoder({
       colorFormats: ['rgb10a2unorm']
     })
-
 
   passEncoder.setPipeline(pipeline);
   const bindGroup = gpuDevice.createBindGroup({
@@ -327,7 +292,6 @@ async function init(options) {
       recordRenderPass(stuff) //pass 2..8
     return state;
   }
-
   return {
     draw,
     canvas: stuff.canvas,
