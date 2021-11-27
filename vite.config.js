8174e234ac55d4181de861b392178160be108272
eggnog <eggnog@nyc-map.com>
2021-11-27 17:29:07 -0600

    save state

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
 const path = require('path')
 const { defineConfig } = require('vite')
 
-// module.exports = defineConfig({
-//   build: {
-//     lib: {
-//       entry: path.resolve(__dirname, 'src/main.js'),
-//       name: 'MyLib',
-//       fileName: (format) => `my-lib.${format}.js`
-//     },
-//     rollupOptions: {
-//       // make sure to externalize deps that shouldn't be bundled
-//       // into your library
-//       external: ['vue'],
-//       output: {
-//         // Provide global variables to use in the UMD build
-//         // for externalized deps
-//         globals: {
-//           vue: 'Vue'
-//         }
-//       }
-//     }
-//   }
-// })
+module.exports = defineConfig({
+  // build: {
+  //   src: {
+  //     entry: path.resolve(__dirname, 'src/main.js'),
+  //     name: 'MyLib',
+  //     fileName: (format) => `my-lib.${format}.js`
+  //   },
+  //   rollupOptions: {
+  //     // make sure to externalize deps that shouldn't be bundled
+  //     // into your library
+  //     external: [''],
+  //     output: {
+  //       // Provide global variables to use in the UMD build
+  //       // for externalized deps
+  //       globals: {
+  //         //simpleGPUCompute: 'SimpleGPUCompute'
+  //       }
+  //     }
+  //   }
+  // }
+})
 
 
 
