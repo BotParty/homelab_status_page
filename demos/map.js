a26210ce660b13470a1cace6a26c9b3db39c6750
_ <93497294+shelbernstein@users.noreply.github.com>
2022-12-14 12:12:47 -0600

    remove netlify-toml

---
+++ demos/map.js
@@ -0,0 +1,9 @@
+//6 triangles 
+//6 lines
+//tesselate
+//map
+
+//and scatter plot
+//20 million cities
+//40 million per city tops - pan and recycle points
+//figure out how much to zoom viewport 1 million points per screen
