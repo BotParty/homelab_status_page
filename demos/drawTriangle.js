96e6016c94e83c742eb98e3f1ff67d54563ceccf
_ <93497294+shelbernstein@users.noreply.github.com>
2022-12-18 19:14:11 -0600

    YAY triangle fuck yeah

--- demos/drawTriangle.js
+++ demos/drawTriangle.js
@@ -10,19 +10,19 @@ const draw = webgpu.initDrawCall({
   // to define them.  No need to manually create shader objects.
   frag: `
   @fragment
-  fn main() -> @location(0) vec4<f32> {
-    return vec4(1.0, 0.0, 1.0, 1.0);
+  fn main_fragment() -> @location(0) vec4<f32> {
+    return vec4(1.0, 0.0, 0.0, 1.0);
   }`,
 
   vert: `
   @vertex
-  fn main(
+  fn main_vertex(
     @builtin(vertex_index) VertexIndex : u32
   ) -> @builtin(position) vec4<f32> {
     var pos = array<vec2<f32>, 3>(
       vec2(0.0, 0.5),
       vec2(-0.5, -0.5),
-      vec2(0.5, -0.5)
+      vec2(0.5, -0.5));
   
     return vec4<f32>(pos[VertexIndex], 0.0, 1.0);
   }`,
