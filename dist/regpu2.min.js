27e9ceb8f15a93b25ccdcc3ac2048f23869f7d34
_ <93497294+shelbernstein@users.noreply.github.com>
2023-01-01 13:33:40 -0600

    test

---
+++ dist/regpu2.min.js
@@ -0,0 +1,12 @@
+// https://simple-webgpu.com v0.19.0 Copyright 2021-2022 Zoox
+(function (global, factory) {
+typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+typeof define === 'function' && define.amd ? define(['exports'], factory) :
+(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.d3 = global.d3 || {}));
+})(this, (function (exports) { 'use strict';
+
+var version = "0.19.0";
+
+exports.version = version;
+
+}));
