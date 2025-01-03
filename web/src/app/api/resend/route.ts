ea89482e6afb5644e6ebafde3ba933b59fd8d747
pau <shel.bernstein123@gmail.com>
2025-01-02 18:19:15 -0600

    simplify

--- web/src/app/api/resend/route.ts
+++ web/src/app/api/resend/route.ts
@@ -7,11 +7,11 @@ const resend = new Resend("re_VisebMXb_GgPww5wnbMNGUdibTYx8HtcX");
 export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'daily_reminder@michael-pollan.app',
-      to: ["eggnog.wahab@gmail.com"],
+      from: 'daily_reminder_2025_2080@michael-pollan.app',
+      //to: ["eggnog.wahab@gmail.com"],
 
-      //to: ["eggnog.wahab@gmail.com", "renafkaufman@gmail.com", "goutham.patnaik@gmail.com", "farahwahab4@gmail.com"],
-      subject: "Daily attempt at reconcilation or collcting a debt from 2025-2085",
+      to: ["eggnog.wahab@gmail.com", "renafkaufman@gmail.com", "goutham.patnaik@gmail.com", "farahwahab4@gmail.com"],
+      subject: "Daily attempt at reconcilation or collcting a debt from 2025 till 2085",
       react: EmailTemplate({ firstName: "rena kaufman" }) as React.ReactElement,
     });
 
