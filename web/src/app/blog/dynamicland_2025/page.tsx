e1de8f9267bd03af5ae8cef68760c7981bf7c573
pau <shel.bernstein123@gmail.com>
2025-01-02 23:32:31 -0600

    simplify

--- web/src/app/blog/dynamicland_2025/page.tsx
+++ web/src/app/blog/dynamicland_2025/page.tsx
@@ -405,7 +405,7 @@ export default function Blog() {
             {/* Hero section */}
             <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
               <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-                Collaborative Thinking Space and Shared Reasoning
+                Collaborative Thinking and Shared Reasoning
               </h1>
               {/* <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
                 A statically generated blog example using{' '}
