da298d1cb8f420e33472035926eaefe346cd6c18
pau <shel.bernstein123@gmail.com>
2025-01-02 18:12:50 -0600

    simplify

--- web/src/app/api/resend/route.ts
+++ web/src/app/api/resend/route.ts
@@ -2,7 +2,7 @@ import { EmailTemplate } from '../../../components/email-template';
 import { Resend } from 'resend';
 import * as React from 'react';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_VisebMXb_GgPww5wnbMNGUdibTYx8HtcX");
 
 export async function POST() {
   try {
@@ -24,3 +24,25 @@ export async function POST() {
     return Response.json({ error }, { status: 500 });
   }
 }
+
+
+export async function GET() {
+    try {
+      const { data, error } = await resend.emails.send({
+        from: 'daily_reminder@michael-pollan.app',
+        to: ["eggnog.wahab@gmail.com"],
+  
+        //to: ["eggnog.wahab@gmail.com", "renafkaufman@gmail.com", "goutham.patnaik@gmail.com", "farahwahab4@gmail.com"],
+        subject: "Daily attempt at reconcilation or collcting a debt from 2025-2085",
+        react: EmailTemplate({ firstName: "rena kaufman" }) as React.ReactElement,
+      });
+  
+      if (error) {
+        return Response.json({ error }, { status: 500 });
+      }
+  
+      return Response.json({ data });
+    } catch (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+  }
