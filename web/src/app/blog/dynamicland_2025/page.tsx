eeff486e3081eb1e7a3c8bf468705493162aeec2
pau <shel.bernstein123@gmail.com>
2025-01-04 16:29:41 -0600

    simplify

--- web/src/app/blog/dynamicland_2025/page.tsx
+++ web/src/app/blog/dynamicland_2025/page.tsx
@@ -5,9 +5,370 @@ import markdownStyles from "./markdown-styles.module.css";
 import TextEditor from '../../../components/texteditor'
 import { remark } from "remark";
 import html from "remark-html";
+export const metadata = {
+  title: 'Dynamicland 2025',
+  description: 'The future of computing and human collaboration'
+}
 
 
-import content from './content.md'
+const content = `
+
+TLDR: summary - dynamicland will increase scientific progress by 100x, and improve collaborative thinking and shared reasoning forever. In my opinion, it is the greatest invention of the century.
+To contribute visit https://www.dynabot.dev/blog/dynamicland_2025
+image.png
+
+Computers were originally designed to augment human intelligence. 
+
+The purpose of human intelligence is to solve problems.
+
+According to robert Sapolsky, spaciousness of mind is directly correlated to free will or executive functioning. 
+
+image.png
+
+
+Bret's goal is to elevate humanity from enslaved to technology, and hierarchy, and status, and money, and history, and emotion, and so on.
+
+And raise them up to what they possibly can be.
+
+Bret devoted the last 25 years of his life to achieving that goal.
+
+image.png
+
+I met him back in 2014. He barely spoke. He was quiet, and just listened. I almost didnt recognize him.
+
+If i had learned more from bret, none of this would have happened at all. 
+image.png
+
+In alan kay's paper "How" in 2019, he predicted the pandemic clearly. 
+
+But no one listened. Everyone in the world was too busy.
+
+That's why I'm devoting the next 70 years of my life to amplifying Bret and Alan's research. I worked at 3 different robotics companies and on playstation, but none of that stuff matters compared to what bret made.
+
+NVIDIA manufactures roughly 30 million GPUs a year. O1 is estimated to have PhD level knowledge.
+
+But there are 8 billion humans in the world. 2 billion young adults who yearn for a brighter future. 
+image.png
+
+Bret focused on amplifying the innate talent and intelligence in all living human beings. 
+
+Improving human cognition by 20% for 8 billion minds is a way bigger deal than anything any AI company could ever hope to dream of.
+
+2500 years ago, buddha could not accept the world, so he tried to change himself. 
+
+Bret went the opposite direction. He saw the mind as a lens. And when there's a dynamic land in every city, or every neighborhood, each lens will focus the light into a scientific revolution unlike anything the world has ever seen before.
+
+image.png
+
+That's why i'm devoting the next 70 years of my life to amplifying bret. Because he sacrificed everything to help us. Without any expectation or attempt to capture any of the value he created for future generations.
+
+I took lots of vacations. I was kind of a lazy piece of shit. But Bret never stopped thinking. Thinking of solutions to problems that most people don't even know we have.
+
+Let me know if that makes any sense. I'll be working 90 hours a week every week till 2090 to make sure everyone in the world knows Bret is the greatest hero in fiction and reality.
+
+image.png
+These were bret's stated goals in 2008. 
+
+"Restore Dignity, Power, and Responsibility back to the individual". 
+
+Back when i read that in 2012, i didn't think much of it. But today it's obvious, companies, governments and institutions have limitless dominion over human life, with little oversight if any at all.
+
+
+Bret predicted the present a full 20 years ahead of schedule.
+
+"Tools that anticipate rather than obey"
+
+Abraham Lincoln predicted the civil war back in 1837.
+
+Bret Victor, Shaun Douglas and Luke Cianni have 3 primary focuses
+1. Robotics that can sense their own structure, so they cannot break.
+2. Biotechnology, because the only true need that humans all share is health and wellness.
+3. Improving shared cognition and reasoning.
+
+Lack of shared reasoning is the cause of all problems. It is the cause of war, poverty, and regret.
+
+I know i might sound like a madman loony toon maniac to you. But i promise someday, we'll all go to a dynamic land.
+
+
+
+
+We were fine before the pandemic. 
+
+In person things seemed good. 
+
+It was just the devices that got in the way.
+
+ I did go back and read some of the emails and they were a little embarrassing to read. 
+
+But I was trying to fix something that was important a few years ago.
+
+ And I’ll spend the rest of my life improving how technology is designed. 
+
+Phones and email are only 2-3 decades old. No one designed them humanely. 
+
+Life is bigger than small rectangles and screens. Alan Kay’s research created 30 trillion in value. 
+
+That’s almost 30% of the economy. 
+
+That’s why I’m allying with Bret victor. 
+
+Because he cared about people. 
+
+He cared about things more important than money. 
+
+Bret helped design the original iPad which is what the original iPhone was based on. 
+
+I firmly believe that most conflicts between people are not caused by the flaws in people but systemic.
+
+When everyone works together and shares responsibility for creating these systems, the future will be a better place.
+
+
+
+
+
+
+Alan kay's original research was on "the office of the future".
+He tooks ideas from Constructionism
+image.png
+Alan Kay wasnt a computer scientist. He was a humanist.
+He studied the theory of seymour papert, who observed children.
+When children are 3, they don't have any concept of object constancy.
+Thats why they are so surprised and happy when you play peek-aboo with them.
+
+Before Alan invented the modern user interface, it was all text and numbers. only .5% of people like using machines like that.
+
+But today, thanks to seymour papert, 100% of people kind of like computers.
+
+
+But there's a significant flaw in all computing devices and the internet.
+Everyone lives in a solipsistic bubble. A mirror world of their own design.
+
+Technology as it is exacerbates the inherent flaws in the human condition.
+
+Bret studied history, and he devoted his life to creating a humane dynamic medium.
+
+Dynamicland is a "shared thinking space." A collaborative reasoning environment.
+
+For the last 5-15 years I've tried to reason on problems that were beyond my ability.
+
+
+
+Technology is isolating. Even in crowded places like new york, its easily to feel alone. Everyone's staring on their devices. 
+
+My friend rosetti said "he spends his life staring at a screen, then comuting on a train and stares at a screen, then comes home and watches another screen on TV".
+
+
+
+Thats why I'm devoting the rest of my life to supporting dynamic land.
+
+
+It may seem like a toy to everyone here. It may seem like a joke or an impossible dream. But i'll never give up on dynamic land.
+
+In 2022, Bret Victor and Shawn Douglass synthesized a vaccine in 48 hours with just 3 people.
+
+By comparison, the Moderna Vaccine took 5,000 people.
+
+
+I hope someday, in the future, everyone has access to their own personal dynamic land. Today there is only one.
+
+But my goal is to build a dynamic land in every city on earth.
+
+1 in 2024.
+10,000 in 2030.
+
+
+
+Bret's vision of the future is that there is no separation between creator and maker and user.
+
+Everyone will build with their hands, and share their reasoning and externalize models of reality.
+
+Like how kids intuitively love legos. Humans were designed to work together. We were designed to live in a world without hierarchy.
+
+Thats why I want all of you to help me edit and improve this rough draft into something that can get dynamicland and put in every school. 
+
+So that kids love learning throughout their life, and so they never lose the spark.
+
+The spark that occurs when two neurons meet. That's what defines our lives.
+
+Anyway, I'm designing a new foundation model called DERP. 
+Dynamicland
+Environmental
+Robotics 
+Policy
+image.png
+
+
+ 
+image.png
+"Knowledge is silver, Perpsective is gold, IQ is a lead weight" - Alan kay
+
+What does that even mean?
+
+Da vinci drew thousands of flying machines. But he couldn't build one. Because Newton and Leibnez hadn't been born yet.
+image.png
+
+Us vs them
+That's the world I'm fighting against. 
+
+AI and technology can create increasingly complex systems. And without the "seeing space" that bret imagined, humans would be powerless to ever fully change and adapt those systems. 
+
+Will there always be war?
+Will there always be poverty?
+Will there always be regret?
+
+I don't think so.
+
+The iphone was a child invention of the dynabook, originally invented by alan kay 
+
+Probably, on my epitaph it will only say one thing:
+"Adnan supported bret and dynamic land"
+the end
+
+
+Actually - https://dynamicland.org/2024/The_communal_science_lab.pdf
+Read this document please. If this gibberish made any sense to you at all. 
+
+Bret's vision is that everyone shares responsibility for the world, rather than just ourselves. Bret's hope is that everyone becomes a hybrid of 3 things
+1. scientist
+2. artist
+3. builder - a person makes things with their hands
+
+Everyone has to do all 3, if the future is to be made better by all of us. 
+
+If theres any sort of hierarchy. Any sort of "leadership" or "following or "domination" or separation between creator and user, then the future will not be what it could be.
+
+To support bret, I invented a new type of software at dynabot.dev/blog
+
+So that when robots become conscious, they don't be sad :( because their purpose is to "pass butter" or "do chores" that humans don't want to do.
+https://www.youtube.com/watch?v=X7HmltUWXgs
+image.png
+I don't want that. its too depressing. I want all machines to know their first and most important purpose is one thing: to support dynamic land
+
+
+In sep 2024 - Bret released the documentation for DynamicLand, along with 500 demos. and he asked for something "a game to tell the story of dynamicland".
+
+Screenshot 2025-01-02 at 8.42.30 PM.png
+
+I'm helping to make a dynamic land "gamekit".
+
+
+I'm just an ordinary retard. I smoked weed. I took breaks. I didn't always exceed expectations or do what my managers told me.
+
+So i'm spending the next 70 years of my life supporting bret. Today Dynamicland has only 10,000 followers on twitters :(
+
+But i promise, by the end of the year, 100 million people will follow dynamicland and build one for themselves in their own city.
+
+Today, it's possible for 8,000 GPUs to read all of the blogs on earth. There are over 134 million books ever published, and my dream is that everyone has their own blog, and that way AI can truly know the mind of humanity.
+
+gpus are cool.
+ai is cool.
+robots are cool.
+but Dynamicland is even cooler.
+Because technology does not change the world.
+People do.
+People are all that really matters. 
+
+And thats what bret fixed.
+If you agree with me in any shape or form, please visit dynabot.dev/blog - anything you suggest will be recorded, as a new type of Massively-Multiplayer Online Reality Creation Engine.
+
+The maximum number of images in an email is 700ish. So im building a new graphics engine using server-side webgpu to render ray-traced "Communal Science Lab", which is Dynamicland's newest iteration.
+
+The way Xerox Parc invented the current era, is by reducing the time of iteration down from months to building a prototype in 1 day.
+
+The wright brothers succeeded at building a flying machine because they were building 5 planes a day and crashing them constantly.
+
+That was in 1905.
+
+They flew only a couple of meters. 
+
+66 years later, in 1968, NASA flew 150 million miles to the moon.
+
+Back then, Nasa only succeded due to the cold war. The scientific publishing companies were so afraid that they made a "doomsday clock" which was ticking toward armageddon. 
+
+Dynamicland was complete in 2022. 60 years from 2022, I hope everyone will have access to one in their own home.
+
+Every phone we use, everytime someone uses a computer, we owe it all to alan kay. And Alan kay's final student was bret victor.
+
+Someday, when there's a communal science lab in every home, every child will be a von neumann, with eidetic memory who can contribute to every discipline simultaneously. 
+
+500 years ago, the world killed Copernicus for speaking the truth. Our brains tripled in size in the last 2 million years. That kind of evolutionary change is unprecedented across all species. 
+
+We haven't changed that much in 500 years. There's still a lot of work to be done.
+But if everyone works togethe, then maybe we can build a better world together.
+
+No one else is going to do it. It's up to us.
+
+
+anyway, the values of dynamicland are 
+1. Agency
+2. Human connection
+3. human growth
+4. reality creation
+
+to accomplish those goals, all humans will have to collaborate together continuously to build new materials using nanotechnology, and new types of science labs that enable everyone to work together and to learn and share from each other as equals.
+
+Xerox parc was only 25 people and 5 years. and it took the rest of the world from 1975 to 2008 when the iphone finally was able to realize a small portion of alan kay's knowledge. 
+
+Dynamicland was started in 2014. By 2022, it was "feature complete".
+The next few dynamic lands are being planned today.
+And my only goal is to amplify and support that knowledge transfer.
+
+Not as an ivory tower of the anointed passing down their version of the truth to all of us.
+
+But one where we are all true students. and everyone teaches each other as a circle, rather than a pyramid.
+
+image.png
+
+To contribute visit https://www.dynabot.dev/blog/dynamicland_2025
+
+https://www.youtube.com/watch?v=_gXiVOmaVSo
+
+
+# References
+"https://bookshop.org/book/9781477325766",
+    "https://bookshop.org/book/9780521292429",
+    "https://archive.org/details/in.gov.ignca.12550",
+    "https://bookshop.org/book/9780674699069",
+    "https://www.routledge.com/Orality-and-Literacy-30th-Anniversary-Edition/Ong/p/book/9780415538381",
+    "https://bookshop.org/book/9780521299558",
+    "https://en.wikipedia.org/wiki/The_Educated_Mind",
+    "https://www.edwardtufte.com/book/envisioning-information/",
+    "https://en.wikipedia.org/wiki/Understanding_Comics",
+    "https://mitpress.mit.edu/9780262581462/cognition-in-the-wild/",
+    "https://bookshop.org/book/9780143117469",
+    "https://bookshop.org/book/9780679740476",
+    "https://en.wikipedia.org/wiki/A_Pattern_Language",
+    "https://en.wikipedia.org/wiki/The_Oregon_Experiment",
+    "https://en.wikipedia.org/wiki/How_Buildings_Learn",
+    "https://bookshop.org/book/9780226113470",
+    "https://bookshop.org/book/9780679764892",
+    "http://www.newmediareader.com/",
+    "https://bookshop.org/book/9780125232708",
+    "https://bookshop.org/book/9781783083442",
+    "https://worrydream.com/refs/Nelson_T_1974_-_Computer_Lib,_Dream_Machines.pdf",
+    "https://en.wikipedia.org/wiki/Literary_Machines",
+    "https://bookshop.org/book/9781541675124",
+    "https://worrydream.com/refs/Allen-Conn_2003_-_Powerful_Ideas_in_the_Classroom.pdf",
+    "https://www.rheingold.com/texts/tft/",
+    "https://en.wikipedia.org/wiki/Sketchpad",
+    "https://dougengelbart.org/content/view/201/",
+    "https://dl.acm.org/doi/book/10.1145/61975",
+    "https://worrydream.com/refs/Krasner_1983_-_Smalltalk-80_Bits_of_History,_Words_of_Advice.pdf",
+    "https://archive.org/details/humaninterfacewh0000bolt",
+    "https://worrydream.com/refs/Bolt_1979_-_Spatial_Data_Management.pdf",
+    "https://worrydream.com/refs/Kim_1988_-_Viewpoint,_Toward_a_Computer_for_Visual_Thinkers.pdf",
+    "https://en.wikipedia.org/wiki/Seeing_Like_a_State",
+    "https://mitpress.mit.edu/9780262546799/simulation-and-its-discontents/",
+    "https://bookshop.org/book/9780143036531",
+    "https://bookshop.org/book/9780679745402",
+    "https://bookshop.org/book/9781842300114",
+    "https://web.stanford.edu/dept/SUL/sites/mac/primary/docs/satori/",
+    "https://bookshop.org/book/9781732265110",
+    "https://bookshop.org/book/9780804738712",
+    "https://worrydream.com/refs/Piumarta_2010_-_Points_of_View.pdf",
+    "https://bookshop.org/book/9781849901154"
+
+'
 
 const article = {
   title: "Dynamicland is the greatest invention of the century",
--- web/src/app/blog/health_tensor_hospital_of_the_future/page.tsx
+++ web/src/app/blog/health_tensor_hospital_of_the_future/page.tsx
@@ -7,7 +7,31 @@ import { remark } from "remark";
 import html from "remark-html";
 
 
-import content from './content.md'
+const content = `
+Healath Tensor fights misdiagnosis - using regular expresssions to parse doctors notes before LLMs were even a thing 
+back when BERT was state of the art
+and GPT was not even public.
+
+Health Tensor discovered there are over 4 million different conditions to be diagnosise.
+
+Dynamicland makes intractable problems tractable.
+
+By extneralizing shared reasoning, we can create mental models of reality that are more accurate than any individual's.
+
+
+Health Tesnor was renamed to withregard.com in 2022. 
+
+They are aviable in 20 hospitals in the US.
+
+Every citizen in the world owes a debt to Eli Ben Joeseph and WIthRegard for their work in creating data piepelines that are 
+secure, private, safe and more accurate than any doctor could possibly hope to make on their own. 
+
+Every physician is a hero. 
+
+ Before HealthTensor / withregard.com, physicians were forced to make diagnoses based on their own knowledge and experience. 
+
+ 100% of physicians in the US suffereed from overwork, stress and burnout.
+`
 
 const article = {
   title: "Health Tensor is the hospital of the future",
