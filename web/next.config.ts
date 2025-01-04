7e17fc85043bbf039daeaabdae47d6e1c2ba4f78
pau <shel.bernstein123@gmail.com>
2025-01-04 16:16:30 -0600

    simplify

--- web/next.config.ts
+++ web/next.config.ts
@@ -12,13 +12,4 @@ const withMDX = require('@next/mdx')({
   // Optionally, you can add MDX-specific config here
 });
 
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.mdx?$/,
-      use: ['babel-loader', '@mdx-js/loader'],
-    });
-    return config;
-  },
-});
+    
