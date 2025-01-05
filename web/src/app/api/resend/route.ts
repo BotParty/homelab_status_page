62b7d58c6b534cf1bc6f1a9d4dfa1ef323e77ee6
pau <shel.bernstein123@gmail.com>
2025-01-04 19:35:40 -0600

    simplify

--- web/src/app/api/resend/route.ts
+++ web/src/app/api/resend/route.ts
@@ -1,26 +1,39 @@
 import { Resend } from 'resend';
 
 const resend = new Resend("re_VisebMXb_GgPww5wnbMNGUdibTYx8HtcX");
+const headers = {
+  'Access-Control-Allow-Origin': '*', // Be more restrictive in production
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+};
+
+
 
 export async function POST(request: Request) {
   try {
-    // Get the JSON data from the request body
+    // Add CORS headers
+  
+    // Handle OPTIONS request (preflight)
+    if (request.method === 'OPTIONS') {
+      return new Response(null, { headers });
+    }
+
     const body = await request.json();
     
     const { data, error } = await resend.emails.send({
       from: 'daily_reminder_2025@michael-pollan.app',
-      to: body.to , // Use data from request or fallback
+      to: body.to,
       subject: body.subject, 
       html: body.html,
     });
 
     if (error) {
-      return Response.json({ error }, { status: 500 });
+      return Response.json({ error }, { status: 500, headers });
     }
 
-    return Response.json({ data });
+    return Response.json({ data }, { headers });
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    return Response.json({ error }, { status: 500, headers });
   }
 }
 
