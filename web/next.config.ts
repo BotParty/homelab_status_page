59770395b44bc53bc069db55b452ec4e3845ebaa
pau <shel.bernstein123@gmail.com>
2025-01-04 20:06:45 -0600

    simplify

--- web/next.config.ts
+++ web/next.config.ts
@@ -5,6 +5,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Configure pageExtensions to include md and mdx
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md'],
+  crossOrigin: 'anonymous',
+
 };
 
 export default nextConfig
