244781b3616dd95978ca91fba03b5f59fd66f2d3
pau <shel.bernstein123@gmail.com>
2025-01-06 21:01:57 -0600

    simplify

--- web/src/app/tools/livekit/page.tsx
+++ web/src/app/tools/livekit/page.tsx
@@ -39,7 +39,7 @@ export default function Page() {
   return (
     <LiveKitRoom
       video={false}
-      audio={true}
+      audio={false}
       token={token}
       screen={true}
       serverUrl={"wss://omnissiah-university-kmuz0plz.livekit.cloud"}
