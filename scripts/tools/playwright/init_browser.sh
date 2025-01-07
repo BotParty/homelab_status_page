5218c5775aae188182bd54608bdd1fe78beb3782
pau <shel.bernstein123@gmail.com>
2025-01-07 11:43:51 -0600

    simplify

---
+++ scripts/tools/playwright/init_browser.sh
@@ -0,0 +1,4 @@
+google-chrome \
+--remote-debugging-port=9222 \
+--user-data-dir=/tmp/chrome-profile \
+--disable-web-security
--- scripts/tools/playwright/linkedin-parse-companies.ts
+++ scripts/tools/playwright/linkedin-parse-companies.ts
@@ -39,3 +39,9 @@ main();
 
 
 //google-chrome --remote-debugging-port=9222
+
+
+google-chrome \
+--remote-debugging-port=9222 \
+--user-data-dir=/tmp/chrome-profile \
+--disable-web-security
