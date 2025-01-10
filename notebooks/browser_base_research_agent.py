dffab54a2df6e9c9901538e8395f619ba0bc1b6c
pau <shel.bernstein123@gmail.com>
2025-01-10 13:42:50 -0600

    simplify

---
+++ notebooks/browser_base_research_agent.py
@@ -0,0 +1,13 @@
+from langchain_openai import ChatOpenAI
+from browser_use import Agent
+import asyncio
+
+async def main():
+    agent = Agent(
+        task="Find a one-way flight from Bali to Oman on 12 January 2025 on Google Flights. Return me the cheapest option.",
+        llm=ChatOpenAI(model="gpt-4o"),
+    )
+    result = await agent.run()
+    print(result)
+
+asyncio.run(main())
