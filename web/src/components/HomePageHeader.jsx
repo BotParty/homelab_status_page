dcd6044f0989d349f17521a4fdae3bb4bc0d4723
pau <shel.bernstein123@gmail.com>
2025-01-10 11:37:06 -0600

    simplify

--- web/src/components/HomePageHeader.jsx
+++ web/src/components/HomePageHeader.jsx
@@ -14,7 +14,7 @@ export default function Header() {
             <a href="https://x.com/adnan_wahab_" className="hover:text-[#8b98e8]">twitter</a>
             <a href="https://linkedin.com/in/adnanwahab" className="hover:text-[#8b98e8]">linkedin</a>
   
-            <a href="/blog" className="hover:text-[#8b98e8]">blog</a> */}
+            <a href="/blog" className="hover:text-[#8b98e8]">blog</a>
           <a href="/tools" className="hover:text-[#8b98e8]">tools</a>
      
           </div>
