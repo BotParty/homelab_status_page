9cfbce8f5b40f6de2979e4e1d2440bb00dbdef12
pau <shel.bernstein123@gmail.com>
2025-01-10 13:47:08 -0600

    simplify

--- blog/index.ts
+++ blog/index.ts
@@ -5,6 +5,7 @@ fs.readdirSync("src").forEach((folder) => {
     const dir = `src/${folder}`;
     fs.readdirSync(`${dir}`).forEach((file) => {
       const fileName = `${dir}/${file}.md`;
+      console.log(fileName);
       // fs.appendFileSync(
       //   `${dir}/${file}.md`,
       //   `https://github.com/BotParty/homelab_status_page/blob/main/[${folder}/${file}](${folder}/${file})\n`,
